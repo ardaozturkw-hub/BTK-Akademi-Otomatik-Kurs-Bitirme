85d2ffeb9df006f3356fbf83155b29ab7471082b
ardaozturkw-hub <ardaozturkw@gmail.com>
2025-10-21 21:59:52 +0300

    Update cookie and course URL in kursBitir.php

--- kursBitir.php
+++ kursBitir.php
@@ -3,8 +3,8 @@
 set_time_limit(-1);
 
 /* BU DEGERLERI DEGISTIRIN*/
-$cookie = "Bu alanı doldurunuz!";
-$kursURL = "https://www.btkakademi.gov.tr/portal/course/css-7453";
+$cookie = "locale=tr; access_token=eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIyODc1ODI1IiwiZG9tYWluIjoiQlRLIiwiaXNzIjoiS0IiLCJwaWQiOjI4NzgyMjUsIm9pZCI6NTEsImV4cCI6MTc2ODA0MzIzMywidXVpZCI6IjBiYzIyMTJhLTRlZDctNGIxNS1hMjNiLTE5NDA1MWQzNWYzYiIsImlhdCI6MTc2MDI2NzIzMywianRpIjoiMWY4ZDU1NjEtOWFlZS00NTMyLWIzMDktODBmNTA1ZDg2ZDVmIn0.Y-tDCzkIGTxVd2MMUi9tyuY46fkUpgm2G8WTSZS7HwhZj42fXI5lVYRQ9QPiUMCj_CWiYMo-tc302WqOO827Trc4BX1_uwmuxtA1IOHjfYbfNdBZzZYIwlkaAjNVyUM8LsiU6FxaspTT4ZXN_uvoP3ydzptVU81gXacPKscjvblhU9DnVVn36UNXD7xnME-U_yxpi37HS5jE8B4FRQAtQcoodw45XF6xDKJYzOde4u9FBtzaJszsQg8mPqpHJwfldMS5eXjrVlqyuuD6mzQHSqcqjwlTocWm1TnR_bF0_ActZQ0Fw649U8h__E2MVi_aH8yIhdcQcgPCs7F7sSpS9w; pacc_id=2875825; pal=true; dtCookie=v_4_srv_1_sn_7FCCF00EBF5BAB7ED45270B0A3251822_perc_100000_ol_0_mul_1_app-3Aa6235131c73ba348_1; _btk_t1=89252937cc57ba1eb4f6071761daa211; rxVisitor=17610588809491E6U0VVKOJN46SHUL645599A3CIRQKMM; account_id=51; _gid=GA1.3.10974422.1761068917; _announcements=1761070106859; _ga=GA1.3.1933822287.1760267178; dtSa=-; rxvt=1761072263280|1761068870106; dtPC=1$70463155_417h-vWBVCLIKARFAVURIJUORWCFMLIFKJVEAO-0e0; _gat=1; _ga_80X7LT6HWG=GS2.3.s1761068879$o4$g1$t1761070463$j60$l0$h0; dtLatC=1";
+$kursURL = "https://www.btkakademi.gov.tr/portal/course/player/deliver/yeni-baslayanlar-icin-python-programlama-26252";
 /* BU DEGERLERI DEGISTIRIN*/
 
 
